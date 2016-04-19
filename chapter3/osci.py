08d9d97155189675a0450e80c94d2a99c85ff2f1
MilCOS <xuhan970130@outlook.com>
2016-04-19 22:23:55 +0800

    完成程序

--- chapter3/osci.py
+++ chapter3/osci.py
@@ -1,6 +1,7 @@
 import math
 import easygui
 from visual import *
+import matplotlib.pyplot as pl
 
 def readfile():
     infile = open('parameter.txt', 'r')
@@ -15,8 +16,6 @@ factor = readfile()
 print factor
 
 class States:
-    global g,l,q,Omega,FD
-    [g,l,q,Omega,FD]=factor[:]
 
 # ini_state=[omega, theta, t]
     def __init__(self, ini_state=[0,0], t=0):
@@ -33,30 +32,34 @@ class States:
 
 class Pendulum:
         
-    def __init__(self, _sta = States(), d_t=0.01):
+    def __init__(self, _sta = States(), d_t=0.01, para=[1.0,2.0,0.2]):
         self.states = []
         self.states.append(_sta)
         self.dt = d_t
         self.keys = self.choosemode()
-        self.modes = Choosemode()
+        self.modes = Choosemode(para)
         
     def Next(self):
         [key_0,key_1,key_2] = self.keys
         states_t = self.states[-1]
         
-        add = [0]
+        add = 0
         if key_0 == 'Simple': 
             add = add + self.modes.Simple(states_t, self.dt)
-        else:
+        if key_0 == 'Not_Simple':
             add = add + self.modes.Not_Simple(states_t, self.dt)
+            print 'pinkie pie'
         if key_1 == 'Drag': 
             add = add + self.modes.Drag(states_t, self.dt)
+            print 'rainbowdash',add
         if key_2 == 'ForceD': 
             add = add + self.modes.ForceD(states_t, self.dt)
-        omega = states_t.omega + add[0]
+        omega = states_t.omega + add
         theta = states_t.theta + omega * self.dt
         t = states_t.t + self.dt
 
+        if (theta <= - math.pi):    theta = theta + math.pi
+        if (theta > math.pi):    theta = theta - math.pi
         self.states.append(States([omega,theta], t))
         return [omega, theta, t]
 #        ball = sphere(pos=vector(x,y,z),radius=0.075,color=color.white)
@@ -76,43 +79,76 @@ class Pendulum:
         
 
 class Choosemode:
-    global g,l,q,omega_D,FD
-    [g,l,q,omega_D,FD]=factor[:]
-    def __init__(self):
+    global g,l
+    [g,l]=factor[:2]
+
+    def __init__(self, para=[1.0,2.0,0.2]):
         pp = 'pinkie pie'
+        self.q = para[0]
+        self.omega_D = para[1]
+        self.FD = para[-1]
 
     def Simple(self, states_t, dt):
         d_omega = - g/l * states_t.theta * dt
-        return [d_omega]
+        return d_omega
 
     def Drag(self, states_t, dt):
-        d_omega = - q * states_t.omega * dt
-        return [d_omega]
+        d_omega = - self.q * states_t.omega * dt
+        return d_omega
 
     def ForceD(self, states_t, dt):
-        d_omega = FD * math.sin(omega_D * states_t.t) * dt
-        return [d_omega]
+        d_omega = self.FD * math.sin(self.omega_D * states_t.t) * dt
+        return d_omega
 
     def Not_Simple(self, states_t, dt):
         d_omega = - g/l * math.sin(states_t.theta) * dt
-        return [d_omega]
+        return d_omega
 
 # ----------
-def release():
+def release(ini_theta = 10, q=1.0, omega_D=2.0, FD=0.2):
+    para = [q, omega_D, FD] # 'package' the parameters
+
     debugger = 0
-    ini_theta = int(easygui.integerbox('Make ini_theta to be')) # degree
+    theta = []
+    t = []
+
     ini_state = [0, ini_theta] 
+    print(ini_theta)
+
     temp = States(ini_state)
-    pendulum_t = Pendulum(temp, 0.01)
+    pendulum_t = Pendulum(temp, 0.01, para)
     counter = 0
     while True:
-        rate(100)
+        rate(2000)
         counter += 1
-        if counter>=1000:
+        if counter >= 1000: #The real time is 10 second now
             debugger += 1
             print debugger
             break
-        pendulum_t.Next()
-    return pendulum_t
+        result = pendulum_t.Next()
+        theta.append(result[1])
+        t.append(result[-1])
+    # waiting for regular plot Ha?
+    ax = pl.axes(xlim=(0, 10), ylim=(-0.4, 0.4))
+    pl.plot(t, theta, label="omega_D="+str(para[0])) #choose i in para[i] to match your changing` parameter
+    pl.legend(loc='lower right')
 
-release()
+def draw(q=1.0, omega_D=2.0, FD=0.2):
+    ini_theta = math.radians(float(easygui.enterbox('Make ini_theta to be in degree'))) # degree shouldn't be zero or the pendulum will keep still
+    fig = pl.figure(figsize=(19,12))
+    pl.title("Oscillator")
+    pl.xlabel(r'$time(s)$', fontsize=20)
+    pl.ylabel(r'$\theta(radians)$', fontsize=20)
+
+    for i in range(len(omega_D)):
+        release(ini_theta, q[i], omega_D[0], FD[0])
+        print omega_D[i]
+        pl.savefig("Pendulum_"+str(i)+".png",dpi=72)
+
+q = [0, 1.0, 2.0]
+omega_D = [3.0,2.0,1.0]
+FD = [0.2, 0.5, 1.0]
+
+
+draw(q, omega_D, FD)
+pl.show()
