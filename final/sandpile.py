71a045809de8dc5c615761ffcba91d6cc7e677d2
Xuhna <MilCOS>
2016-06-18 18:35:16 +0800

    进一步完善程序,准备画图

--- final/sandpile.py
+++ final/sandpile.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import cm
 from matplotlib import pyplot as plt
 
 def boundary_con(Z_temp):
@@ -11,16 +12,14 @@ def pickadd():
     i,j = L * np.random.uniform(0,1,size=2)
     i,j = int(i),int(j)
     Z1[i,j] += 1
-    Z1[i-1,j] -= 1
-    Z1[i,j-1] -= 1
+#    Z1[i-1,j] -= 1
+#    Z1[i,j-1] -= 1
 
 def evolve(rc):
     global Z1,pinkie
     pinkie = np.zeros([L,L])
     counter = 0
-    pickadd()
     while (np.sum(np.where(Z1>rc)) > 0):
-        Z1 = boundary_con(Z1)
         for i in range(1,L-1):
             for j in range(1,L-1):
     #            Z1[i,j] += 2
@@ -33,13 +32,25 @@ def evolve(rc):
                     Z1[i+1,j] += 1
                     Z1[i-1,j] += 1
                     Z1[i,j-1] += 1
-    #                twili[i,j] -= 1
                     pinkie[i,j] += 1
                 else: pass
+        Z1 = boundary_con(Z1)
     return counter
 
-L = 100 #size of the sandpile
+def draw(Z_temp):
+    fig, ax = plt.subplots()
+    cax = ax.imshow(Z_temp, interpolation='nearest', cmap=cm.coolwarm)
+    ax.set_title('Sandpile with vertical colorbar')
+    # Add colorbar, make sure to specify tick locations to match desired ticklabels
+    cbar = fig.colorbar(cax, ticks=[0, 4, 1], drawedges=True)
+    cbar.ax.set_yticklabels(['0', '1', '2', '3'])  # vertically oriented colorbar
+    plt.show()
+    
+
+
+L = 50 #size of the sandpile
 rc = 3 # Give the threshold value
+N = 200
 
 xs = np.arange(0,L,1)
 ys = np.arange(0,L,1)
@@ -47,13 +58,21 @@ x,y = np.meshgrid(xs,ys)
 
 pinkie = np.zeros([L,L])
 twili = np.ones([L,L])
-Z1 = twili*int(rc*np.random.uniform(0,2))
-note = 0
-
-for k in range(10):
-    note += evolve(rc)
-
-#plt.scatter(x, y, Z1)
-plt.scatter(x, y, pinkie)
-print(note)
-plt.show()
+Z1 = np.random.random_integers(rc-1,rc+1,size=[L,L])
+note1,note2 = 0,0
+for k in range(7000):
+    pickadd()
+note1 = evolve(rc)
+note2 += note1
+draw(Z1)
+D = []
+for k in range(N):
+    pickadd()
+    note1 = evolve(rc)
+    note2 += note1
+    #print(note1, note2)
+    D.append(note1)
+    pinkie[np.where(pinkie>0)] = 1
+    twili += pinkie 
+draw(twili)
+print(float(N)/sum(D))
